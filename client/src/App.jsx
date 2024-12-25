10d8cad1c30e0c98eb05e12ae8541c38e11c3b5b
Puripat-Haru <pr.puripat@gmail.com>
2024-12-25 13:10:14 +0700

    2nd commit

---
+++ client/src/App.jsx
@@ -0,0 +1,20 @@
+// rafce
+import React from 'react'
+import Navbars from './components/Navbars'
+import Hero from './components/Hero'
+import About from './components/About'
+import Experience from './components/Experience'
+
+
+const App = () => {
+  return (
+    <div>
+      <Navbars />
+      <Hero />
+      <About />
+      <Experience />
+    </div>
+  )
+}
+
+export default App
