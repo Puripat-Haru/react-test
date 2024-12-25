56f5e73bf6754f4f404732920bcb743f74fdfb1b
Puripat-Haru <pr.puripat@gmail.com>
2024-12-25 13:57:30 +0700

    3rd commit

--- client/src/components/Navbars.jsx
+++ client/src/components/Navbars.jsx
@@ -1,6 +1,7 @@
-import { Fragment } from 'react'
 import { Disclosure, DisclosureButton, DisclosurePanel, Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react'
 import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
+
+import pooh from '../assets/pooh.jpg'
 
 const navigation = [
   { name: 'About', href: '#about', current: true },
@@ -69,7 +70,7 @@ export default function Navbars() {
                   <span className="sr-only">Open user menu</span>
                   <img
                     alt=""
-                    src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                    src={pooh}
                     className="size-8 rounded-full"
                   />
                 </MenuButton>
